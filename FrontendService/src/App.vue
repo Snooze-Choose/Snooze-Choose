f8bbf0c53f545fce1b25589e246063773b1c0ad1
Markus <60973893+markusgerman@users.noreply.github.com>
2025-01-22 10:56:26 +0100

    add product list

--- FrontendService/src/App.vue
+++ FrontendService/src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import ProductList from './components/ProductList.vue';
+import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
 <div class="flex flex-wrap gap-4">
-
+  <Navbar></Navbar>
   <ProductList></ProductList>
 
 </div>
