a834e03382674879244873231149ad070fe1713b
Markus <60973893+markusgerman@users.noreply.github.com>
2025-02-12 17:19:30 +0100

    add vite env

--- AdminFrontendService/vite.config.ts
+++ AdminFrontendService/vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import autoprefixer from 'autoprefixer'
 import tailwind from 'tailwindcss'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   css: {
